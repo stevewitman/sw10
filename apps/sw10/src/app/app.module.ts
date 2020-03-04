790de6544e8666947e97da25f2a037fd618347b9
Steve Witman <stevewitman@gmail.com>
2020-03-04 07:21:48 -0700

    Added angular material and bootstrap 4 grid

--- apps/sw10/src/app/app.module.ts
+++ apps/sw10/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MaterialModule } from '@bb/material';
+import { CoreDataModule } from '@bb/core-data';
+import { UiToolbarModule } from '@bb/ui-toolbar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    MaterialModule,
+    CoreDataModule,
+    UiToolbarModule,
+    BrowserAnimationsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
