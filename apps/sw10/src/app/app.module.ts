cf858fb00a554146dabee436a753bbcfd26dcd6c
Steve Witman <stevewitman@gmail.com>
2020-03-04 07:29:55 -0700

    Generated projects service, projects component, projects-list and project-details component, and toolbar-component

--- apps/sw10/src/app/app.module.ts
+++ apps/sw10/src/app/app.module.ts
@@ -6,10 +6,16 @@ import { MaterialModule } from '@bb/material';
 import { CoreDataModule } from '@bb/core-data';
 import { UiToolbarModule } from '@bb/ui-toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProjectsComponent } from './projects/projects.component';
+import { ProjectsListComponent } from './projects/projects-list/projects-list.component';
+import { ProjectDetailsComponent } from './projects/project-details/project-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProjectsComponent,
+    ProjectsListComponent,
+    ProjectDetailsComponent
   ],
   imports: [
     BrowserModule,
